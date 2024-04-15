b282153737f43f6f677f65f153ee4493c4b5bf6b
Johann Martinache <johann.martinache@sky.uk>
2024-04-15 09:28:58 +0100

    feat(lazygit): lazygit configuration

--- nvim/lua/autocmd/init.lua
+++ nvim/lua/autocmd/init.lua
@@ -16,14 +16,6 @@ function autocmd.load_autocmds()
 		command = "checktime",
 	})
 
-	local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = "*.go",
-		callback = function()
-			require("go.format").goimport()
-		end,
-		group = format_sync_grp,
-	})
 end
 
 autocmd.load_autocmds()
--- nvim/lua/modules/lsp/plugins.lua
+++ nvim/lua/modules/lsp/plugins.lua
@@ -65,7 +65,7 @@ function lsp.packer()
 			local null_ls = require("null-ls")
 
 			null_ls.setup({
-			    debug = true,
+				debug = true,
 				sources = {
 					null_ls.builtins.completion.spell,
 
@@ -95,14 +95,10 @@ function lsp.packer()
 					null_ls.builtins.formatting.prettierd,
 					null_ls.builtins.formatting.stylua,
 				},
-			})
-
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			require("null-ls").setup({
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
 						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
+						vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
 							group = augroup,
 							buffer = bufnr,
 							callback = function()
