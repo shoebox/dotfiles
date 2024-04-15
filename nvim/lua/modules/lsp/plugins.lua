f4cee40f7c9e3832be0476f8c93a5167d0ab1f14
Johann Martinache <johann.martinache@sky.uk>
2024-04-15 09:29:29 +0100

    feat(neovim): adds code actions sources to `none-ls`

--- nvim/lua/modules/lsp/plugins.lua
+++ nvim/lua/modules/lsp/plugins.lua
@@ -65,9 +65,13 @@ function lsp.packer()
 			local null_ls = require("null-ls")
 
 			null_ls.setup({
-				debug = true,
 				sources = {
 					null_ls.builtins.completion.spell,
+
+					-- code actions
+					null_ls.builtins.code_actions.impl,
+					null_ls.builtins.code_actions.refactoring,
+					null_ls.builtins.code_actions.statix,
 
 					-- diagnostics
 					null_ls.builtins.diagnostics.ansiblelint,
