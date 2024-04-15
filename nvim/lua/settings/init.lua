715f35f72c4f5562cbbeb41a8c1ca756311fe35b
Johann Martinache <johann.martinache@sky.uk>
2024-04-15 10:57:54 +0100

    refactor(neovim): moves the theme definition to the plugin itself

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -1,11 +1,5 @@
-
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-require("catppuccin").setup()
-vim.cmd [[colorscheme tokyonight]]
-
 vim.cmd([[
     let g:floaterm_keymap_toggle = '<F7>'
     let g:floaterm_width = 0.9
     let g:floaterm_height = 0.9
 ]])
-
