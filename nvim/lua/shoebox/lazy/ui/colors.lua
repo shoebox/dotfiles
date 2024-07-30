5500194c40d68a78ff7cdac645b82fb02b0719b6
Johann Martinache <johann.martinache@sky.uk>
2024-07-30 11:15:19 +0100

    feat(neovim/shoebox/lazy/ui): color tweaks for tokyonight decorated telescope

--- nvim/lua/shoebox/lazy/ui/colors.lua
+++ nvim/lua/shoebox/lazy/ui/colors.lua
@@ -1,58 +1,74 @@
 function MyColorScheme(color)
-    color = color or "tokyonight-night"
-    vim.cmd.colorscheme(color)
+	color = color or "tokyonight-night"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                dim_inactive = false,
-                style = "storm",
-                styles = {
-                    sidebars = "dark",
-                    floats = "dark",
-                },
-                terminal_colors = true,
-                -- transparent = true,
-            })
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				dim_inactive = false,
+				style = "storm",
+				styles = {
+					-- sidebars = "dark",
+					-- floats = "dark",
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+				on_highlights = function(hl, c)
+					-- hl.Normal = "Foo"
+					do
+						return
+					end
+					local prompt = "#2d3149"
+					hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg }
+					hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+					hl.TelescopePromptNormal = { bg = prompt }
+					hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+					hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
+					hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+					hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
+				end,
+				terminal_colors = true,
+				-- transparent = true,
+			})
 
-            vim.cmd.colorscheme("tokyonight-night")
-            MyColorScheme()
-        end,
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "loctvl842/monokai-pro.nvim",
-        config = function()
-            require("monokai-pro").setup()
-        end,
-        lazy = true,
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup()
-        end,
-        lazy = true,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end,
-        lazy = true,
-    },
+			vim.cmd.colorscheme("tokyonight-night")
+			MyColorScheme()
+		end,
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"loctvl842/monokai-pro.nvim",
+		config = function()
+			require("monokai-pro").setup()
+		end,
+		lazy = true,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup()
+		end,
+		lazy = true,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
+		end,
+		lazy = true,
+	},
 }
