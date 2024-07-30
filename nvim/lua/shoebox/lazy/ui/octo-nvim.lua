1374bf19b82ece990af3434ca80f9902a9bf3d1a
Johann Martinache <johann.martinache@sky.uk>
2024-07-30 11:17:34 +0100

    feat(neovim/shoebox/ui): adds `octo.nvim` for testing

---
+++ nvim/lua/shoebox/lazy/ui/octo-nvim.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		cmd = { "Octo" },
+		config = function()
+			require("octo").setup()
+		end,
+	},
+}
