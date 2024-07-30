56305b86823213871fc57b03b81f255a0123ee7c
Johann Martinache <johann.martinache@sky.uk>
2024-07-30 11:16:39 +0100

    feat(neovim/shoebox/ui): better indent blankline whitespace

--- nvim/lua/shoebox/lazy/ui/indent-blankline.lua
+++ nvim/lua/shoebox/lazy/ui/indent-blankline.lua
@@ -2,8 +2,16 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
+			local highlight = {
+				"VertSplit",
+			}
+
 			require("ibl").setup({
+				indent = { highlight = highlight },
 				scope = { enabled = false },
+				whitespace = {
+					remove_blankline_trail = false,
+				},
 			})
 		end,
 		event = { "BufRead", "BufNewFile" },
