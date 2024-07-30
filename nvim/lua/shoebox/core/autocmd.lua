2fd611ac0e8d5c1f2e87b0427967f9e5026faff8
Johann Martinache <johann.martinache@sky.uk>
2024-07-30 11:11:07 +0100

    feat(neovim/lua/shoebox/core): disable focuslost autocomd for now

--- nvim/lua/shoebox/core/autocmd.lua
+++ nvim/lua/shoebox/core/autocmd.lua
@@ -1,10 +1,10 @@
 local focus_group = vim.api.nvim_create_augroup("focus_group", { clear = true })
 
-vim.api.nvim_create_autocmd({ "FocusLost" }, {
-	group = focus_group,
-	pattern = { "*" },
-	command = ":wa",
-})
+-- vim.api.nvim_create_autocmd({ "FocusLost" }, {
+-- 	group = focus_group,
+-- 	pattern = { "*" },
+-- 	command = ":wa",
+-- })
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
 	group = focus_group,
