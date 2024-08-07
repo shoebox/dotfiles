1e690c8740388a626467b4aa94489c8d8ce6f3ff
Johann Martinache <johann.martinache@sky.uk>
2024-08-07 16:30:31 +0100

    feat(neovim/shoebox/core): autocmd to write on focus lost

--- nvim/lua/shoebox/core/autocmd.lua
+++ nvim/lua/shoebox/core/autocmd.lua
@@ -1,13 +1,43 @@
-local focus_group = vim.api.nvim_create_augroup("focus_group", { clear = true })
+local vim = vim
 
--- vim.api.nvim_create_autocmd({ "FocusLost" }, {
--- 	group = focus_group,
--- 	pattern = { "*" },
--- 	command = ":wa",
--- })
+vim.o.autoread = true
 
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+local focus_group = vim.api.nvim_create_augroup("focus_group", {})
+
+local exclude = { "neo-tree", "kulala://ui" }
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 	group = focus_group,
 	pattern = { "*" },
-	command = "checktime",
+	callback = function(args)
+		if not vim.bo[args.buf].readonly then
+			local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+			local ignore_buftype = {
+				"acwrite",
+				"filesystem",
+				"nofile",
+				"nowrite",
+				"prompt",
+				"terminal",
+			}
+			local ignore_buffername = { "", " " }
+			if not vim.tbl_contains(ignore_buftype, buftype) then
+				local bufferName = vim.api.nvim_buf_get_name(0)
+				if not vim.tbl_contains(ignore_buffername, bufferName) then
+					vim.cmd.w({ mods = { silent = true } })
+				end
+			end
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+	group = focus_group,
+	callback = function()
+		local bufferName = vim.api.nvim_buf_get_name(0)
+		if not vim.tbl_contains(exclude, bufferName) then
+			vim.cmd("checktime")
+		end
+	end,
+	pattern = { "*" },
 })
